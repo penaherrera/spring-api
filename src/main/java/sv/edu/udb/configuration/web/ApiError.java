289dd7063344c6408b1a7f8bf0fdee469f4bdc9c
cpena-minery <carlos.cpa26@gmail.com>
2024-10-11 11:30:43 -0600

    ad ding api error and api error wrappe classes

---
+++ src/main/java/sv/edu/udb/configuration/web/ApiError.java
@@ -0,0 +1,17 @@
+package sv.edu.udb.configuration.web;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+/** ApiError DTO **/
+@Getter
+@Setter
+@Builder
+public class ApiError {
+    private Integer status;
+    private String type;
+    private String title;
+    @Builder.Default
+    private String source = "base";
+    private String description;
+}
